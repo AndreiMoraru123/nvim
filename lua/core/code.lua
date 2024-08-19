9116e8c0d8f27b4a16167c9b827b58a8a764c466
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2024-08-19 19:37:18 +0300

    change raw strings to api calls

--- lua/core/code.lua
+++ lua/core/code.lua
@@ -1,83 +1,74 @@
+local vscode = require('vscode')
+
 -- Run python file
-vim.keymap.set('n', '<leader>py', "<Cmd>lua require('vscode').action('python.execInTerminal')<CR>",
+vim.keymap.set('n', '<leader>py', function() vscode.action('python.execInTerminal') end,
     { noremap = true, silent = true })
 
 -- Debug python file
-vim.keymap.set('n', '<leader>pd', "<Cmd>lua require('vscode').action('python.debugInTerminal')<CR>",
+vim.keymap.set('n', '<leader>pd', function() vscode.action('python.debugInTerminal') end,
     { noremap = true, silent = true })
 
 -- Continue debugging
-vim.keymap.set('n', '<leader>c',
-    "<Cmd>lua require('vscode').action('workbench.action.debug.continue')<CR>",
+vim.keymap.set('n', '<leader>c', function() vscode.action('workbench.action.debug.continue') end,
     { noremap = true, silent = true })
 
 -- Step over (next)
-vim.keymap.set('n', '<leader>n',
-    "<Cmd>lua require('vscode').action('workbench.action.debug.stepOver')<CR>",
+vim.keymap.set('n', '<leader>n', function() vscode.action('workbench.action.debug.stepOver') end,
     { noremap = true, silent = true })
 
 -- Step into
-vim.keymap.set('n', '<leader>s',
-    "<Cmd>lua require('vscode').action('workbench.action.debug.stepInto')<CR>",
+vim.keymap.set('n', '<leader>s', function() vscode.action('workbench.action.debug.stepInto') end,
     { noremap = true, silent = true })
 
 -- Step out
-vim.keymap.set('n', '<leader>o',
-    "<Cmd>lua require('vscode').action('workbench.action.debug.stepOut')<CR>",
+vim.keymap.set('n', '<leader>o', function() vscode.action('workbench.action.debug.stepOut') end,
     { noremap = true, silent = true })
 
--- jump to cursor
-vim.keymap.set('n', '<leader>j',
-    "<Cmd>lua require('vscode').action('debug.jumpToCursor')<CR>",
-    { noremap = true, silent = true })
+-- Jump to cursor
+vim.keymap.set('n', '<leader>j', function() vscode.action('debug.jumpToCursor') end, { noremap = true, silent = true })
 
 -- Restart debugging
-vim.keymap.set('n', '<leader>r',
-    "<Cmd>lua require('vscode').action('workbench.action.debug.restart')<CR>",
+vim.keymap.set('n', '<leader>r', function() vscode.action('workbench.action.debug.restart') end,
     { noremap = true, silent = true })
 
 -- Stop debugging
-vim.keymap.set('n', '<leader>q',
-    "<Cmd>lua require('vscode').action('workbench.action.debug.stop')<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>q', function() vscode.action('workbench.action.debug.stop') end,
+    { noremap = true, silent = true })
 
 -- Set breakpoint
-vim.keymap.set('n', '\\b',
-    "<Cmd>lua require('vscode').action('editor.debug.action.toggleBreakpoint')<CR>",
+vim.keymap.set('n', '\\b', function() vscode.action('editor.debug.action.toggleBreakpoint') end,
     { noremap = true, silent = true })
 
 -- Format document
-vim.keymap.set('n', '<leader>f',
-    "<Cmd>lua require('vscode').action('editor.action.formatDocument')<CR>", { noremap = true, silent = true })
-
--- Test at cursor
-vim.keymap.set('n', '<leader>t', "<Cmd>lua require('vscode').action('testing.runAtCursor')<CR>",
+vim.keymap.set('n', '<leader>f', function() vscode.action('editor.action.formatDocument') end,
     { noremap = true, silent = true })
 
+-- Test at cursor
+vim.keymap.set('n', '<leader>t', function() vscode.action('testing.runAtCursor') end, { noremap = true, silent = true })
+
 -- Debug test at cursor
-vim.keymap.set('n', '<leader>dt', "<Cmd>lua require('vscode').action('testing.debugAtCursor')<CR>",
+vim.keymap.set('n', '<leader>dt', function() vscode.action('testing.debugAtCursor') end,
     { noremap = true, silent = true })
 
 -- Test current file
-vim.keymap.set('n', '<leader>tf', "<Cmd>lua require('vscode').action('testing.runCurrentFile')<CR>",
+vim.keymap.set('n', '<leader>tf', function() vscode.action('testing.runCurrentFile') end,
     { noremap = true, silent = true })
 
 -- Debug tests in current file
-vim.keymap.set('n', '<leader>dtf', "<Cmd>lua require('vscode').action('testing.debugCurrentFile')<CR>",
+vim.keymap.set('n', '<leader>dtf', function() vscode.action('testing.debugCurrentFile') end,
     { noremap = true, silent = true })
 
 -- Test all
-vim.keymap.set('n', '<leader>tt', "<Cmd>lua require('vscode').action('testing.runAll')<CR>",
-    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tt', function() vscode.action('testing.runAll') end, { noremap = true, silent = true })
 
 -- Debug all tests
-vim.keymap.set('n', '<leader>dtt', "<Cmd>lua require('vscode').action('testing.debugAll')<CR>",
-    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>dtt', function() vscode.action('testing.debugAll') end, { noremap = true, silent = true })
 
 -- Configure python tests
-vim.keymap.set('n', '<leader>pt', "<Cmd>lua require('vscode').action('python.configureTests')<CR>",
+vim.keymap.set('n', '<leader>pt', function() vscode.action('python.configureTests') end,
     { noremap = true, silent = true })
 
 -- Zen mode
-vim.keymap.set('n', '<leader>zz',
-    "<Cmd>lua require('vscode').action('workbench.action.toggleZenMode')<CR>",
+vim.keymap.set('n', '<leader>zz', function() vscode.action('workbench.action.toggleZenMode') end,
     { noremap = true, silent = true })
+
