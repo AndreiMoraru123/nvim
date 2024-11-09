29676e5af1168b591ae526e8bbf1633c4ef80b4c
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2024-11-09 23:03:30 +0200

    ctrl + b for buffers and remove unused telescope builtins

--- lua/core/lazy/telescope.lua
+++ lua/core/lazy/telescope.lua
@@ -6,8 +6,6 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
         vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>ps', builtin.grep_string, {})
+        vim.keymap.set('n', '<C-b>', builtin.buffers, {})
     end
 }
