acf896f00c70f8c0578e0ae5274f5b68bebe217f
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2026-03-17 09:11:22 +0200

    update ts with new api

--- lua/core/lazy/treesitter.lua
+++ lua/core/lazy/treesitter.lua
@@ -1,35 +1,16 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-
     build = ":TSUpdate",
-
     cond = not vim.g.vscode,
-
     config = function()
-        require 'nvim-treesitter.configs'.setup {
-            -- A list of parser names, or "all"
+        require('nvim-treesitter').setup {
             ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "python", "bash", "prolog",
                 "javascript", "typescript", "rust", "go", "jsonc", "markdown", "markdown_inline"
             },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
             auto_install = true,
-
-            -- List of parsers to ignore installing (or "all")
-            -- ignore_install = { "javascript" },
-
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-
-            indent = {
-                enable = true,
-            },
+            highlight = { enable = true, additional_vim_regex_highlighting = false },
+            indent = { enable = true },
         }
     end
 }
