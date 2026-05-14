be95b17b74dd30c0312d9b58e96d386e9c74c433
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2026-05-14 10:23:22 +0300

    change theme to norrsken

--- lua/core/lazy/colorscheme.lua
+++ lua/core/lazy/colorscheme.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-    color = color or "vesper"
+    color = color or "norrsken"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -7,12 +7,11 @@ function ColorMyPencils(color)
 end
 
 return {
-    "datsfilipe/vesper.nvim",
+    "webhooked/norrsken.nvim",
     cond = not vim.g.vscode,
     config = function()
-        vim.cmd("colorscheme vesper")
-        require('vesper').setup({
-            disable_background = true,
+        require('norrsken').setup({
+            transparent_bg = true,
         })
         ColorMyPencils()
     end
